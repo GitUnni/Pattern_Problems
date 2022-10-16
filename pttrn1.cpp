ae7636c37186e69fe9a95add8167f80db993d5ef
Akash A S <114794304+GitUnni@users.noreply.github.com>
2022-10-16 11:52:20 +0530

    Good repo
    Nice repo

---
+++ pttrn1.cpp
@@ -0,0 +1,19 @@
+/*Printing pattern
+*****
+*****
+*****
+                                          */
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+      int i, j;
+      for (i = 1; i <= 3; i++)
+      {
+            for (j = 1; j <= 5; j++)
+            {
+                  cout << ("*");
+            }
+            cout << "\n";
+      }
+}
---
+++ pttrn2.cpp
@@ -0,0 +1,15 @@
+//Printing pattern **** **** ****
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+      int i,j;
+      for(i=1;i<=14;i++)
+      {
+            if(i%5==0)
+            cout<<" ";
+            else
+            cout<<"*";
+      }
+      
+}
